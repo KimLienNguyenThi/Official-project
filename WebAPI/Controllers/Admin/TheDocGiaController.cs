fe529de74d5d81a51e3e42847c8a9ef8dcc89314
Vo Gia Huy <vogiahuy.cntt@gmail.com>
2024-11-11 11:04:30 +0700

    Add Account + TheDocGia + ThongTinTheDocGia

---
+++ WebAPI/Controllers/Admin/TheDocGiaController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Areas.Admin.Data;
+using WebAPI.DTOs.Admin_DTO;
+using WebAPI.Models;
+using WebAPI.Services.Admin;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebAPI.Controllers.Admin
+{
+    [Route("api/admin/[controller]/[action]")]
+    [ApiController]
+    public class TheDocGiaController : ControllerBase
+    {
+        private readonly TheDocGiaService _theDocGiaService;
+
+        public TheDocGiaController(TheDocGiaService theDocGiaService)
+        {
+            _theDocGiaService = theDocGiaService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTheDocGia()
+        {
+            try
+            {
+                List<DTO_DocGia_TheDocGia> listTheDocGia = _theDocGiaService.GetAllTheDocGia();
+
+                return Ok(new APIResponse<List<DTO_DocGia_TheDocGia>>()
+                {
+                    Success = true,
+                    Message = "Lấy dữ liệu thành công",
+                    Data = listTheDocGia
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult ThongTinTheDocGia(int id)
+        {
+            try
+            {
+                DTO_DocGia_TheDocGia thongTinDocGia = _theDocGiaService.GetById(id);
+
+                if (thongTinDocGia != null)
+                {
+                    return Ok(new APIResponse<DTO_DocGia_TheDocGia>()
+                    {
+                        Success = true,
+                        Message = "Lấy dữ liệu thành công",
+                        Data = thongTinDocGia
+                    });
+                }
+                else
+                {
+                    return Ok(new APIResponse<DTO_DocGia_TheDocGia>()
+                    {
+                        Success = false,
+                        Message = "Không tìm thấy độc giả",
+                        Data = thongTinDocGia
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Update([FromBody] DTO_DocGia_TheDocGia obj)
+        {
+            try
+            {
+                if (_theDocGiaService.Update(obj))
+                    return Ok();
+                else
+                    return BadRequest("Không cập nhật được! Lỗi từ server");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult DangKyTheDocGia([FromBody] DTO_DocGia_TheDocGia obj)
+        {
+            try
+            {
+                if (_theDocGiaService.DangKyTheDocGia(obj))
+                {
+                    return Ok(new APIResponse<object>()
+                    {
+                        Success = true,
+                        Message = "Đăng ký thành công",
+                        Data = null 
+                    });
+                }
+                else
+                {
+                    return Ok(new APIResponse<object>()
+                    {
+                        Success = false,
+                        Message = "Số điện thoại đã tồn tại",
+                        Data = null
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+    }
+}
--- WebApp/Areas/Admin/Controllers/ThongTinDocGiaController.cs
+++ WebApp/Areas/Admin/Controllers/ThongTinDocGiaController.cs
@@ -1,175 +1,181 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using WebApp.Admin.Data;
 using WebApp.Areas.Admin.Data;
+using WebApp.Models;
 
 namespace WebApp.Areas.Admin.Controllers
 {
     [Area("admin")]
     [Route("admin/thongtindocgia")]
-   // [Authorize(AuthenticationSchemes = "AdminCookie")]
+    [Authorize(AuthenticationSchemes = "AdminCookie")]
 
     public class ThongTinDocGiaController : Controller
     {
-        // Constructor của lớp DangKyMuonSachController (có thể để trống vì chưa cần API)
+        Uri baseAddress = new Uri("https://localhost:7028/api/admin");
+        private readonly HttpClient _client;
+
         public ThongTinDocGiaController()
         {
-            // Không cần khởi tạo HttpClient vì chưa có API
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+
         }
 
-        // Action để hiển thị trang đăng ký mượn sách
-        public IActionResult Index()
+
+        [Route("")]
+        public async Task<IActionResult> Index()
         {
-            return View(); // Trả về view Index.cshtml
+            if (User.IsInRole("QuanLyKho"))
+            {
+                return RedirectToAction("LoiPhanQuyen", "phanquyen");
+            }
+            else
+            {
+                HttpResponseMessage response = await _client.GetAsync(_client.BaseAddress + "/TheDocGia/GetAllTheDocGia");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string dataJson = response.Content.ReadAsStringAsync().Result;
+                    var apiResponse = JsonConvert.DeserializeObject<APIResponse<List<DTO_DocGia_TheDocGia>>>(dataJson);
+
+                    if (apiResponse != null && apiResponse.Success)
+                    {
+                        var data = apiResponse.Data;
+                        ViewData["ThongTinDocGia"] = data;
+                        return View();
+                    }
+                    else
+                    {
+                        return View();
+                    }
+                }
+                else
+                {
+                    return View();
+                }
+            }
         }
 
 
-        // Bạn có thể thêm các action khác cho chức năng như tạo đơn đăng ký, xem đơn, v.v.
-        // Ví dụ:
-        
-        //Uri baseAddress = new Uri("https://localhost:7028/api/admin");
-        //private readonly HttpClient _client;
+        [HttpGet]
+        [Route("GetAllThongTinDocGia")]
+        public ActionResult GetAllThongTinDocGia()
+        {
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/TheDocGia/GetAllTheDocGia").Result;
 
-        //public ThongTinDocGiaController()
-        //{
-        //    _client = new HttpClient();
-        //    _client.BaseAddress = baseAddress;
+            if (response.IsSuccessStatusCode)
+            {
+                string dataJson = response.Content.ReadAsStringAsync().Result;
+                var apiResponse = JsonConvert.DeserializeObject<APIResponse<List<DTO_DocGia_TheDocGia>>>(dataJson);
 
-        //}
-
-        //[Route("")]
-        //public IActionResult Index()
-        //{
-        //    if (User.IsInRole("QuanLyKho"))
-        //    {
-        //        return RedirectToAction("LoiPhanQuyen", "phanquyen");
-        //    }
-        //    else
-        //    {
-        //        List<DTO_DocGia_TheDocGia> data = new List<DTO_DocGia_TheDocGia>();
-
-        //        HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/TheDocGia/GetAllTheDocGia").Result;
-
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            string dataJson = response.Content.ReadAsStringAsync().Result;
-        //            data = JsonConvert.DeserializeObject<List<DTO_DocGia_TheDocGia>>(dataJson);
-
-        //            ViewData["ThongTinDocGia"] = data;
-        //            return View();
-        //        }
-        //        else
-        //        {
-        //            return View();
-        //        }
-        //    }
+                if (apiResponse != null && apiResponse.Success)
+                {
+                    return Json(new { success = true, data = apiResponse.Data });
+                }
+                else
+                {
+                    return Json(new { success = false, Message = apiResponse.Message });
+                }
+            }
+            else
+            {
+                return Json(new { success = false, message = response.Content.ReadAsStringAsync() });
+            }
+        }
 
 
-        //}
+        [HttpPost]
+        [Route("ThongTinTheDocGia")]
+        public ActionResult ThongTinTheDocGia(int id)
+        {
+            try
+            {
+                DTO_DocGia_TheDocGia theDocGia = new DTO_DocGia_TheDocGia();
+
+                HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + $"/TheDocGia/ThongTinTheDocGia/{id}").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string dataJson = response.Content.ReadAsStringAsync().Result;
+                    var apiResponse = JsonConvert.DeserializeObject<APIResponse<DTO_DocGia_TheDocGia>>(dataJson);
+
+                    if (apiResponse != null && apiResponse.Success)
+                    {
+                        // Trả về dữ liệu JSON nếu thành công
+                        return Json(new { success = true, data = apiResponse.Data });
+                    }
+                    else
+                    {
+                        // Trả về thông báo lỗi nếu không tìm thấy sách
+                        return Json(new { success = false, message = apiResponse.Message });
+                    }
+                }
+                else
+                {
+                    return Json(new { success = false, message = response.Content.ReadAsStringAsync() });
+                }
 
 
-        //[HttpGet]
-        //[Route("GetAllThongTinDocGia")]
-        //public ActionResult GetAllThongTinDocGia()
-        //{
-        //    List<DTO_DocGia_TheDocGia> data = new List<DTO_DocGia_TheDocGia>();
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi nếu có
+                Console.WriteLine($"Error in ChiTietDocGia: {ex.Message}");
+                return Json(new { success = false, message = "Đã xảy ra lỗi" });
+            }
+        }
 
-        //    HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/TheDocGia/GetAllTheDocGia").Result;
+        [HttpPost]
+        [Route("CapNhatThongTin")]
+        public ActionResult CapNhatThongTin(int maDocGia, DateOnly ngaySinh, string diaChi, string gioiTinh, string soDienThoai, string tenDocGia)
+        {
+            try
+            {
+                DocGium dg = new DocGium();
 
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        string dataJson = response.Content.ReadAsStringAsync().Result;
-        //        data = JsonConvert.DeserializeObject<List<DTO_DocGia_TheDocGia>>(dataJson);
+                dg.Madg = maDocGia;
+                dg.Hotendg = tenDocGia;
+                dg.Ngaysinh = ngaySinh;
+                dg.Diachi = diaChi;
+                dg.Gioitinh = gioiTinh;
+                dg.Sdt = soDienThoai;
 
-        //        return Json(new { success = true, data = data });
-        //    }
-        //    else
-        //    {
-        //        return Json(new { success = false, message = "Lỗi gọi API" });
-        //    }
-        //}
+                // call API
+                HttpResponseMessage response = _client.PostAsJsonAsync(_client.BaseAddress + "/ThongTinDocGia/UpdateThongTinDocGia", dg).Result;
 
+                if (response.IsSuccessStatusCode)
+                {
+                    string dataJson = response.Content.ReadAsStringAsync().Result;
+                    var apiResponse = JsonConvert.DeserializeObject<APIResponse<DocGium>>(dataJson);
 
-        //[HttpPost]
-        //[Route("ThongTinTheDocGia")]
-        //public ActionResult ThongTinTheDocGia(int id)
-        //{
-        //    try
-        //    {
-        //        DTO_DocGia_TheDocGia theDocGia = new DTO_DocGia_TheDocGia();
+                    if(apiResponse != null && apiResponse.Success)
+                    {
+                        return Json(new { success = true, data = dg });
+                    }
+                    else
+                    {
+                        return Json(new { success = false, message = apiResponse.Message });
+                    }
+                }
+                else
+                {
+                    return Json(new { success = false, message = response.Content.ReadAsStringAsync() });
+                }
 
-        //        HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + $"/TheDocGia/ThongTinTheDocGia/{id}").Result;
+            }
+            catch (Exception ex)
+            {
+                // Kiểm tra xem lỗi có phải do tồn tại số điện thoại không
+                if (ex.Message.Contains("Số điện thoại đã tồn tại."))
+                    return Json(new { success = false, message = "Số điện thoại đã tồn tại." });
 
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            string dataJson = response.Content.ReadAsStringAsync().Result;
-        //            theDocGia = JsonConvert.DeserializeObject<DTO_DocGia_TheDocGia>(dataJson);
-
-        //            if (theDocGia != null)
-        //            {
-        //                // Trả về dữ liệu JSON nếu thành công
-        //                return Json(new { success = true, data = theDocGia });
-        //            }
-        //            else
-        //            {
-        //                // Trả về thông báo lỗi nếu không tìm thấy sách
-        //                return Json(new { success = false, message = "Không tìm thấy độc  giả" });
-        //            }
-        //        }
-        //        else
-        //        {
-        //            return Json(new { success = false, message = "Gọi API không thành công" });
-        //        }
-
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Xử lý lỗi nếu có
-        //        Console.WriteLine($"Error in ChiTietDocGia: {ex.Message}");
-        //        return Json(new { success = false, message = "Đã xảy ra lỗi" });
-        //    }
-        //}
-
-        //[HttpPost]
-        //[Route("CapNhatThongTin")]
-        //public ActionResult CapNhatThongTin(int maDocGia, DateOnly ngaySinh, string diaChi, string gioiTinh, string soDienThoai, string tenDocGia)
-        //{
-        //    try
-        //    {
-        //        DocGium dg = new DocGium();
-
-        //        dg.MaDg = maDocGia;
-        //        dg.HoTenDg = tenDocGia;
-        //        dg.NgaySinh =  ngaySinh;
-        //        dg.DiaChi = diaChi;
-        //        dg.GioiTinh = gioiTinh;
-        //        dg.Sdt = soDienThoai;
-
-        //        // call API
-        //        HttpResponseMessage response = _client.PostAsJsonAsync(_client.BaseAddress + "/ThongTinDocGia/UpdateThongTinDocGia", dg).Result;
-
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //        return Json(new { success = true, data = dg });
-        //        }
-        //        else
-        //        {
-        //            return Json(new { success = false, message = response.RequestMessage });
-        //        }
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Kiểm tra xem lỗi có phải do tồn tại số điện thoại không
-        //        if (ex.Message.Contains("Số điện thoại đã tồn tại."))
-        //            return Json(new { success = false, message = "Số điện thoại đã tồn tại." });
-
-        //        // Xử lý lỗi nếu có
-        //        Console.WriteLine($"Error: {ex.Message}");
-        //        return Json(new { success = false, message = "Đã xảy ra lỗi" });
-        //    }
-        //}
+                // Xử lý lỗi nếu có
+                Console.WriteLine($"Error: {ex.Message}");
+                return Json(new { success = false, message = "Đã xảy ra lỗi" });
+            }
+        }
 
 
     }
