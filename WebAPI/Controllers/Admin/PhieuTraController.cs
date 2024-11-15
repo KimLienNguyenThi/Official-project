97755126833eb0608b607948ffb346009bd082a1
KimLien <6251071052@st.utc2.edu.vn>
2024-11-15 09:20:52 +0700

    modal

---
+++ WebAPI/Controllers/Admin/PhieuTraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.Admin
+{
+    public class PhieuTraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
